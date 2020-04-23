a4ddf1f4749cbd401d46bcb0c870658942355ec3
dbmartin00 <david.martin@split.io>
2020-04-23 09:08:38 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import off from './OFF_bulb.png';
+import on from './ON_bulb.png';
+import control from './control.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { SplitFactory, SplitTreatments } from '@splitsoftware/splitio-react';
+
+const MyComponentV0 = () => ( <img src={control} alt="CONTROL"/> );
+const MyComponentV1 = () => ( <img src={off} alt="OFF"/> );
+const MyComponentV2 = () => ( <img src={on} alt="ON"/> );
+
+class MyComponent extends React.Component {
+  renderContent(treatmentWithConfig, props) {
+    const { treatment, config } = treatmentWithConfig;
+    if (treatment === 'on') {
+    	return (<MyComponentV2 config={config} {...props} />);
+    } else if (treatment === 'off') {
+    	return (<MyComponentV1 config={config} {...props} />);
+    } else {
+      return (<MyComponentV0 config={config} {...props} />);
+    }
+  }
+  render(props) {
+    return (
+      <SplitTreatments names={['new_onboarding']} >
+        {({ treatments, isReady }) => { // Injects a TreatmentsWithConfig object and a boolean isReady flag.
+          return isReady ?
+            this.renderContent(treatments['new_onboarding'], props) : // Use the treatments and configs.
+            <p>not ready</p>; // who has a Spinner?!!
+        }}
+      </SplitTreatments>
+    );
+  }
 }
-
+const sdkConfig = {
+    core: {
+        authorizationKey: "3m17636s132i2krt28o982cbe0o0qpi0honr",
+        key: "dmartin"
+    },
+    scheduler: {
+        featuresRefreshRate: 1
+    }
+};
+class App extends React.Component {
+	render() {
+		return (
+			<SplitFactory config={sdkConfig} updateOnSdkUpdate={true}>
+	      <MyComponent />
+	    </SplitFactory>
+		);
+	}
+}
 export default App;
